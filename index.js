7f8f7683c06001d2a1553f480b5f489ce993bdd2
nirkaufman <nirkaufman@gmail.com>
2021-03-14 11:20:40 +0200

    render element

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+// REACT DOM -> take object and create DOM Element
+// out of it
+function renderElement(element) {
+  const {type, props, children} = element;
+
+  if(typeof(type) === 'string') {
+    const domElement = document.createElement(type);
+
+    children.forEach( child => {
+      if(typeof(child) === 'string') {
+        const textNode = document.createTextNode(child)
+        domElement.appendChild(textNode);
+      } else {
+        domElement.appendChild(renderElement(child));
+      }
+    })
+
+
+    return domElement;
+  }
+
+}
+
+// REACT -> utility to create tree of objects (element)
 function createElement(type, props, ...children) {
   return {type, props, children};
 }
 
-const userInfo = createElement('container', null,
-    createElement('title', {id: 5}, 'Nir Kaufman'),
-    createElement('subTitle', null, 'Nir Kaufman'),
-    createElement('container', null,
-        createElement('text', null, 'Street name'),
-        createElement('zip', null, '123456'),
+// Runtime
+const userInfo = createElement('div', null,
+    createElement('h1', {id: 5}, 'Nir Kaufman'),
+    createElement('h2', null, 'Nir Kaufman'),
+    createElement('div', null,
+        createElement('p', null, 'Street name'),
+        createElement('p', null, '123456'),
     ),
 )
 
 
-console.log(userInfo);
+console.log(renderElement(userInfo));
 
